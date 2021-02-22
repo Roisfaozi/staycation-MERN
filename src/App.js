42f4ae8dd6fea8919b985df3ea0edce0031c3ac4
Rois Faozi <67546196+Roisfaozi@users.noreply.github.com>
2021-02-22 08:26:23 +0700

    Create booking page and chekout page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'assets/scss/style.scss'
 import DetailPage from 'pages/DetailPage';
 import LandingPage from 'pages/LandingPage';
+import Checkout from 'pages/Checkout';
 import Example from 'pages/Example';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Route exact path='/' component={LandingPage}></Route>
         <Route exact path='/properties/:id' component={DetailPage}></Route>
+        <Route path='/checkout' component={Checkout}></Route>
         <Route path='/example' component={Example}></Route>
       </Router>
     </div>
---
+++ src/element/Setepper/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import propTypes from 'prop-types'
+
+
+export default function Stepper(props) {
+
+    const { steps, initialStep } = props
+    const stepKeys = Object.keys(steps)
+
+    const [CurrentStep, setCurrentStep] = useState(
+        stepKeys.indexof(initialStep) > -1 ? initialStep : stepKeys[0]
+    )
+    const totalStep = stepKeys.length
+    const indexStep = stepKeys.indexOf(CurrentStep)
+
+    function prevStep() {
+        if (+indexStep > 0) setCurrentStep(stepKeys[indexStep - 1])
+    }
+
+    function nexStep() {
+        if (+indexStep < totalStep) setCurrentStep(stepKeys[indexStep + 1])
+    }
+
+    return <>(props.children(prevStep, nexStep, CurrentStep, steps))</>
+}
+
+Stepper.propTypes = {
+    data: propTypes.object.isRequired,
+    initialStep: propTypes.string
+}
